2f40a826d46bce174f763b47ca565e9e9d239024
VismarkJuarez <vismarkjuarez@gmail.com>
2020-03-17 13:32:38 -0500

    Wrote logic for calculating real-estate prices based on a single property attribute list.

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
+#instantiating a LinearRegression object
+linearRegressionModel = LinearRegression()
+
 def perform_linear_regression():
     df = pd.read_csv('resources/USA_Housing.csv') #Read in the USA_Housing.csv file, located within the `/resources` folder
     print(df.info()) #printing info to the console.
@@ -33,9 +36,6 @@ def perform_linear_regression():
     '''
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
 
-    #instantiating a LinearRegression object
-    linearRegressionModel = LinearRegression()
-
     #fitting the model using the attributes and the target values
     linearRegressionModel.fit(X_train, y_train)
 
@@ -48,13 +48,38 @@ def perform_linear_regression():
     #predicting on a hard-coded entry:
     print('X_test is: \n')
     print(X_test.head())
+    test_size = len(X_test)
+    print('size of X_test is: {}'.format(test_size))
 
     print('X_test prediction results: \n')
     print(predictions)
+    predictions_size = len(predictions)
+    print('size of predictions is: {}'.format(predictions_size))
 
 
 #Runs the linear regression model function at start up
 perform_linear_regression()
+
+'''
+      Avg. Area Income  Avg. Area House Age  Avg. Area Number of Rooms  Avg. Area Number of Bedrooms  Area Population
+1718       66774.99582             5.717143                   7.795215                          4.32      36788.98033
+2511       62184.53937             4.925758                   7.427689                          6.22      26008.30912
+345        73643.05730             6.766853                   8.337085                          3.34      43152.13958
+2521       61909.04144             6.228343                   6.593138                          4.29      28953.92538
+54         72942.70506             4.786222                   7.319886                          6.41      24377.90905
+'''
+def predictPriceEstimate(property:Property) -> []:
+    #Creating a 2D array with the property data.  The array needs to be a 2D array since this is the format the predictor expects the data in
+    propertyData = [[property.averageAreaIncome, property.averageAreaHouseAge, property.averageAreaNumberOfRooms, property.averageAreaNumberOfBedrooms, property.areaPopulation]]
+
+    #Feeding the propertyData 2D array into the classifier.  This will yield a price estimate (prediction)
+    priceEstimate2DArray = linearRegressionModel.predict(propertyData)
+
+    #Only one prediction will be made, meaning there will only exist one item in the 2D array -- the first elemet of the first array in the 2D array.
+    priceEstimate = priceEstimate2DArray[0][0]
+
+    #return the estimated price
+    return priceEstimate
 
 
 @app.route('/', methods=['GET'])
@@ -94,4 +119,7 @@ def consumeUserInput():
     receivedDataAsJSON = request.get_json()
     print('received data is: {}'.format(receivedDataAsJSON))
     property = deserializePropertyData(receivedDataAsJSON)
-    return 'SUCCESS!'
+    estimate = predictPriceEstimate(property)
+
+    #Return the estimated price, as a JSON formatted string
+    return "{ " + "estimate: {}".format(estimate) + " }"
